16b0315c36fcfd86751027d880bab7ae8f3042d9
hnguyen013 <hanhnguyen130526@gmail.com>
2025-12-06 14:35:30 +0700

    feat:view books

--- library.py
+++ library.py
@@ -2,11 +2,21 @@ library = []
 def add_book(title, author):
     library.append({"title": title, "author": author, "is_available": True})
     print(f"Book '{title}' by {author} has been added!")
+def view_books():
+    if not library:
+        print("Library is empty.")
+        return
+    print("\n===== ALL BOOKS =====")
+    for index, book in enumerate(library, start=1):
+        title = book.get("title", "Unknown Title")
+        author = book.get("author", "Unknown Author")
+        available = "Yes" if book.get("is_available", True) else "No"
+        print(f"{index}. {title} — {author} (Available: {available})")
 def main():
     while True:
         print("\n===== LIBRARY MENU =====")
         print("1. Add book")
-        print("2. Show library")
+        print("2. View All Books")
         print("3. Exit")
         choice = input("Enter your choice: ")
         if choice == "3":
@@ -16,6 +26,8 @@ def main():
             title = input("Enter book title: ")
             author = input("Enter book author: ")
             add_book(title, author)
+        elif choice =="2":
+            view_books()
 
 if __name__ == "__main__":
     main()
